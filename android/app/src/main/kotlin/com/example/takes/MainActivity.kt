c9e05bdf58a93dbe545f467db9388dce586d0518
Kevin van Dijk <kevinvd05@gmail.com>
2025-03-29 17:17:16 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/takes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.takes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Takes',
+      home: Scaffold(body: Center(child: Text('Hello, world!'))),
+    ),
+  );
+}
